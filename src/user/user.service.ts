3877beca2a8f6b8fdc3925197c3993b68e0d10a1
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-19 13:56:55 +0200

    Prenventing duplicate users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -9,9 +9,24 @@ export class UserService {
     constructor(@InjectRepository(User) private repo: Repository<User>){}
 
 
-    async createUser(username: string, email: string, hashedPassword: string){
+    async createUser(username: string, email: string, hashedPassword: string):Promise<number|null>{
 
-        const user = await this.repo.query("INSERT INTO user (email, password, username) VALUES (?,?,?)", [email, hashedPassword, username])
+        let user:number;
+
+        try{
+
+            user = await this.repo.query("INSERT INTO user (email, password, username) VALUES (?,?,?)", [email, hashedPassword, username])
+        } catch(error){
+
+            console.log(error.driverError);
+
+            if (error.driverError.code === 'SQLITE_CONSTRAINT') {
+                console.log('error triggered');
+            }
+            
+            throw new ConflictException("Username or Email are already taken")
+        }
+
 
 
         return user;
