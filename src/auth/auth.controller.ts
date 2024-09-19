71f6ada5256d466e464606a1ff8de24f199685d0
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-19 10:57:37 +0200

    Added input validation and injected auth service in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(private autService: AuthService){}
 
-    @Post("/signup")
-    async signup(){
+    @Post("/register")
+    async signup(@Body() body: RegisterDto){
+        console.log(body)
+
+        return body;
 
     }
 
