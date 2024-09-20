63b772bcf6f6560a0810ec0e3847d737a46859ad
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-20 14:57:35 +0200

    handling of env varibles

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { UserService } from './user/user.service';
 import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
-import { UserController } from './user/user.controller';
 import { ListsModule } from './lists/lists.module';
 import { List } from './lists/list.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "db.sqlite",
-      entities: [User, List],
-      synchronize: true,
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService)=>{
+          // Allows us to change the db based on the enviornment
+        return {
+          type: "sqlite",
+          database: "db.sqlite",
+          entities: [User, List],
+          synchronize: true
+        }
+      }
     }),
     UserModule, AuthModule, ListsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, UserService, AuthService],
+  providers: [AppService, UserService, AuthService, JwtService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guards';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret, //TODO: Change to secure env value
-      signOptions: { expiresIn: '2h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get('JWTSECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
