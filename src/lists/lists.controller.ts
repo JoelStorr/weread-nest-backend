78de78bf3c13f345916c80fe4ff91c5730328c90
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-22 16:27:43 +0200

    Clean up

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,40 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { newListDto } from './dtos/newList.dto';
 
 @Controller('lists')
 export class ListsController {
+  constructor(private listsService: ListsService) {}
 
-    constructor(private listsService: ListsService){}
+  @Get('/')
+  async getLists(@Request() req: any) {
+    const userId = req.user.sub;
 
+    return await this.listsService.loadAllLists(userId);
+  }
 
-    @Get("/")
-    async getLists(@Request() req: any){
+  @Post('/createlist')
+  async createList(@Body() body: newListDto, @Request() req: any) {
+    const userId = req.user.sub;
+    const listId = await this.listsService.createList(
+      userId,
+      body.name,
+      body.privateList,
+    );
+    return listId;
+  }
 
-        const userId = req.user.sub;
+  @Delete('/:id')
+  async deleteList(@Param('id') id: string, @Request() req: any) {
+    const userId = req.user.sub;
 
-        return await this.listsService.loadAllLists(userId);
-    }
+    let numId = Number(id);
 
-    @Post("/createlist")
-    async createList(@Body() body:newListDto, @Request() req: any ){
-        const userId = req.user.sub;
-        const listId = await this.listsService.createList(userId, body.name, body.privateList);
-        return listId;
-    }
+    const deletedListId = await this.listsService.deleteList(userId, numId);
 
-
-    @Delete("/:id")
-    async deleteList(@Param("id") id: string, @Request() req:any){
-
-        const userId = req.user.sub;
-
-        let numId = Number(id)
-
-        const deletedListId = await this.listsService.deleteList(userId, numId);
-
-        return deletedListId;
-
-    }
-
+    return deletedListId;
+  }
 }
