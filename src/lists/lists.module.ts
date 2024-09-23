908e1a78b23bed343f85d4729f74da990917c49b
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-23 18:46:07 +0200

    Clean up

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -6,10 +6,7 @@ import { List } from './list.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([List]),
-    
-  ],
+  imports: [TypeOrmModule.forFeature([List])],
   providers: [ListsService],
   controllers: [ListsController],
   exports: [ListsService, TypeOrmModule],
