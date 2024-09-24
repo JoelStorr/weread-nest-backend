76e787cf49c305b82e9686681d68968e7778f6db
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-24 15:37:05 +0200

    Clean up

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,33 +1,35 @@
-import { List } from "src/lists/list.entity";
-import { AfterInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-
+import { List } from 'src/lists/list.entity';
+import {
+  AfterInsert,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({unique: true})
+  @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
 
-  @Column({unique: true})
+  @Column({ unique: true })
   username: string;
 
-  @Column({nullable: true})
+  @Column({ nullable: true })
   name!: string;
 
-  @OneToMany(()=>List, (list)=>list.user)
+  @OneToMany(() => List, (list) => list.user)
   lists: List[];
 
-
   @AfterInsert()
-  afterInser(){
+  afterInser() {
     //TODO: Creates bas list for the user
     // TODO: Creates statistic entry for the user
   }
-
-
 }
